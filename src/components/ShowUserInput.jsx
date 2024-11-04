37ecfd0df7d91629daa42305a0f939f2a7e326d5
Anirudha Bele <beleanirudha8@gmail.com>
2024-11-04 08:34:33 +0530

    create 'ShowUserInput.jsx' component

---
+++ src/components/ShowUserInput.jsx
@@ -0,0 +1,24 @@
+import { useState } from "react";
+
+const ShowUserInput = () => {
+	const [userName, setUserName] = useState("");
+
+	return (
+		<div>
+			<form action="#">
+				<label htmlFor="userName">Enter your name:</label>
+				<input
+					type="text"
+					name="userName"
+					id="userName"
+					value={userName}
+					onChange={(e) => setUserName(e.target.value)}
+				/>
+			</form>
+			<div className="showInput">
+				<p>User name is: {userName}</p>
+			</div>
+		</div>
+	);
+};
+export default ShowUserInput;
